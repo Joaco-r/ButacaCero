c4b4f5eaea52f361ee95030c10fc70ac432d9bba
Joaco-r <rave111301@gmail.com>
2026-09-11 17:44:40 -0300

    Small refactor

--- django/butacaCero/settings.py
+++ django/butacaCero/settings.py
@@ -21,10 +21,6 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
