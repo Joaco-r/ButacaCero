2da20db4f7818be7cb0693efe7dc0fb9cb53bd08
Joaco-r <rave111301@gmail.com>
2026-09-11 17:41:28 -0300

    contected django to posgres db

--- django/butacaCero/settings.py
+++ django/butacaCero/settings.py
@@ -14,8 +14,11 @@ from pathlib import Path
 import environ
 import os
 
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env(os.path.join("/", ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
